a73bcc7e8af2dfcd3ce50ac4ef263ac265a0fc46
Preet <preetpanchani1@gmail.com>
2024-12-28 10:11:41 +0000

    Modified it to NOT INCLUDE Large files in the notebook folder

---
+++ hateSpeechIdentification/entity/artifact_entity.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+# Data Ingestion artifacts
+@dataclass
+class DataIngestionArtifacts:
+    imbalance_data_file_path: str
+    raw_data_file_path: str
+
+
+
+# Data Transformation artifacts
+@dataclass
+class DataTransformationArtifacts:
+    transformed_data_path : str
+
+
+@dataclass
+class ModelTrainerArtifacts: 
+    trained_model_path:str
+    x_test_path: list
+    y_test_path: list
+
+
+
+@dataclass
+class ModelEvaluationArtifacts:
+    is_model_accepted: bool 
+
+
+
+@dataclass
+class ModelPusherArtifacts:
+    bucket_name: str
+
+
+
+
+
+
+
+
+
